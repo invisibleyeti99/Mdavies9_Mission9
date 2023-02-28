b8c2330fa89519749c2ce5c3a0c10ba8fc670a6e
Matthew Davies <mattjdavies199@gmail.com>
2023-02-28 12:58:02 -0700

    Add project files.

---
+++ Mdavies9_Mission9/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Mdavies9_Mission9.Models;
+using Mdavies9_Mission9.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mdavies9_Mission9.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set;  }
+      
+        public HomeController(BookstoreContext x) => context = x;
+        public IActionResult Index( int pageNum = 1)
+        {
+            int NumResults = 10;
+            var rx = new BooksVM
+            {
+                Books = context.Books.OrderBy(x => x.Title).Skip((pageNum - 1) * NumResults).Take(NumResults),
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = context.Books.Count(),
+                    BooksPerPage = NumResults,
+                    CurrPage = pageNum
+                    
+                    
+
+                }
+            };
+            return View(rx);
+        }
+    }
+}
---
+++ Mdavies9_Mission9/Models/ViewModels/BooksVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mdavies9_Mission9.Models.ViewModels
+{
+    public class BooksVM
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
